5b4cb020825d1ee1a26d4bfce4d254db91dc0be1
Brusssssss <155645813+Brusssssss@users.noreply.github.com>
2024-11-25 14:24:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("docente.json");
+const router = jsonServer.router("usuarios.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
